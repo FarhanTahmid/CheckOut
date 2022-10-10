3fe9f0699302e8a0cba41b34f0012a7af6d08c5d
FarhanTahmid <farhantahmid881@gmail.com>
2022-10-10 12:55:27 +0600

    firebase check

--- checkout_app/views.py
+++ checkout_app/views.py
@@ -1,4 +1,20 @@
+from distutils.command.config import config
+from django.db import DatabaseError
 from django.shortcuts import render,redirect
+import pyrebase
+
+config={
+    "apiKey": "AIzaSyCs-CW9vsD3jWGQOfyDznnvo5bg60g0gHo",
+    "authDomain": "checkouttest-c5528.firebaseapp.com",
+    "databaseURL": "https://checkouttest-c5528-default-rtdb.firebaseio.com",
+    "projectId": "checkouttest-c5528",
+    "storageBucket": "checkouttest-c5528.appspot.com",
+    "messagingSenderId": "686410994548",
+    "appId": "1:686410994548:web:33493b9b0ce36cee2b9221",
+}
+firebase=pyrebase.initialize_app(config)
+auth=firebase.auth()
+database=firebase.database()
 
 # Create your views here.
 def homepage(request):
@@ -6,6 +22,7 @@ def homepage(request):
     return render(request, 'index.html')
 def signUp(request):
     '''Goes to signup form from index page'''
+    print(f"Website name is {database.child('Data').child('Name').get().val()}")
     return render(request,'signUpForm.html')
 def login(request):
     '''Goes to login form from signup page upon signup true. accessible from index'''
